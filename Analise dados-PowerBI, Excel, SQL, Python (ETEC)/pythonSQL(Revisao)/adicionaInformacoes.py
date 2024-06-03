871d67f38e1edef5da03a4a30ed441a074774911
KaiqueVfreitas <kaique.vieira2502eu@outlook.com>
2024-06-03 16:26:39 -0300

    Enviando arquivo do curso de analise de dados para o github que estava no notebook

---
+++ Analise dados-PowerBI, Excel, SQL, Python (ETEC)/pythonSQL(Revisao)/adicionaInformacoes.py
@@ -0,0 +1,64 @@
+import pyodbc
+from datetime import datetime
+import re
+
+dados_conexao = (
+    "Driver={SQL Server};"
+    "Server=DESKTOP-QQL8NG8\SQLSERVERDEV;"
+    "Database=PythonSQL;"
+)
+
+conexao = pyodbc.connect(dados_conexao)
+print("Conexão Bem Sucedida")
+
+cursor = conexao.cursor()
+
+def converter_data(data):
+    return datetime.strptime(data, '%d/%m/%Y').strftime('%Y-%m-%d')
+
+def validar_data(data):
+    data = data.strip()
+    if not re.match(r'^\d{2}/\d{2}/\d{4}$', data):
+        raise ValueError("Formato de data inválido. Use DD/MM/AAAA.")
+    data_formatada = datetime.strptime(data, '%d/%m/%Y')
+    data_minima = datetime(2019, 10, 10)
+    data_maxima = datetime(2024, 1, 1)
+    return data_minima <= data_formatada < data_maxima
+
+def cadastrar_produto(cliente=None):
+    if cliente is None:
+        cliente = input("Nome do cliente: ")
+
+    while True:
+        produto = input("Produto comprado: ")
+        data_venda = input("Data da venda (dd/mm/yyyy): ").strip()
+        if validar_data(data_venda):
+            data_venda_formatada = converter_data(data_venda)
+            break
+        else:
+            print("Data inválida! A data deve ser igual ou maior que 10/10/2019 e menor que 01/01/2024.")
+
+    preco = float(input("Preço unitário: "))
+    quantidade = int(input("Quantidade: "))
+
+    comandoInsert = """
+    INSERT INTO Vendas (cliente, produto, data_venda, preco, quantidade)
+    VALUES (?, ?, ?, ?, ?)
+    """
+
+    cursor.execute(comandoInsert, (cliente, produto, data_venda_formatada, preco, quantidade))
+    conexao.commit()
+
+    print("Dados inseridos com sucesso!")
+
+cliente = input("Nome do cliente: ")
+cadastrar_produto(cliente)
+
+while True:
+    continuar = input("Deseja cadastrar outro produto para o mesmo cliente? (s/n): ").strip().lower()
+    if continuar != 's':
+        break
+    cadastrar_produto(cliente)
+
+cursor.close()
+conexao.close()
---
+++ Analise dados-PowerBI, Excel, SQL, Python (ETEC)/pythonSQL(Revisao)/pythonSQL(Revisao).sql
@@ -0,0 +1,13 @@
+create database PythonSQL;
+use PythonSQL;
+
+
+CREATE TABLE Vendas (
+    id_venda int primary key IDENTITY(3,1),
+    cliente varchar(50) not null,
+    produto varchar(50) not null,
+    data_venda date not null,
+    preco decimal(11, 2) not null,
+    quantidade int not null
+);
+
